bbf74f2fa8c3180a6544f547b00874eebbfc54db
pgrrr <pgrrr119@gmail.com>
2024-02-18 17:00:50 +0900

    init: first commit

---
+++ src/main/java/com/pgrrr/springbasetemplate/SpringBaseTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.pgrrr.springbasetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgrrr/springbasetemplate/SpringBaseTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgrrr.springbasetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaseTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
